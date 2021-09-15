eeb94c75b7176c0f33989adbf451a399421b33c0
Jimmy <jwu4477@gmail.com>
2021-09-14 23:53:37 -0400

    Added part 2 of Flixster assignment.

--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -3,16 +3,26 @@ package com.example.flixter.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+    String release;
+    int movieId;
 
+
+    //empty constructor needed by parcel library
+    public Movie() {
+
+    }
 
     //take in json object and read out the fields we need
     public Movie(JSONObject jsonObject) throws JSONException {
@@ -20,6 +30,9 @@ public class Movie {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        release = jsonObject.getString("release_date");
+        movieId = jsonObject.getInt("id");
 
     }
 
@@ -47,5 +60,17 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
